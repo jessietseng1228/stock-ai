7fd376ebef86345b82f07fc3d1e9f0ac1b953c40
jessie tseng <jessie.yc.tseng@gmail.com>
2026-07-05 00:49:04 +0800

    fix render port binding

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET"])
+def home():
+    return "OK", 200
+
 @app.route("/", methods=["POST"])
 def webhook():
-    print("收到LINE事件")
-
-    # ⭐一定要立刻回應
     return "OK", 200
 
 
-@app.route("/", methods=["GET"])
-def home():
-    return "OK"
-
-
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
